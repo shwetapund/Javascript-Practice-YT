e1ce2349d9e8bef0944302aa20858cc6439df9b1
shwetapund <pundshweta2020@gmail.com>
2024-04-05 09:58:52 -0700

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+//console.log(typeof(score)); //method
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "36" => 36
+// "34abc" => NaN (not a number) Nan ka type Number he hein
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "sdfghj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shweta" => true
+
+let someNumber = 35
+
+let stringNum = String(someNumber)
+console.log(typeof stringNum);
