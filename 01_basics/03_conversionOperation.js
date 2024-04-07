0e2d7263fb384e9340eb04cc1623a12b7ada8272
shwetapund <pundshweta2020@gmail.com>
2024-04-06 23:48:18 -0700

    comparison opertion practic

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,7 +18,7 @@ let isLoggedIn = "sdfghj"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -27,4 +27,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 35
 
 let stringNum = String(someNumber)
-console.log(typeof stringNum);
+//console.log(typeof stringNum);
+
+// **************************************** Operations *******************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2*2);
+// console.log(2**2); //** laganes  */
+// console.log(2-2);
+// console.log(2/7);
+
+let v1 = "hii"
+let v2 = " shweta"
+
+let v3 = v1 + v2
+//console.log(v3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + "2")
+// console.log((1 + 2 )+ "2");
+// console.log("1" + 2 + 2);
+
+gameCounter = 200
+console.log(gameCounter);
+gameCounter--;
+console.log(gameCounter);
