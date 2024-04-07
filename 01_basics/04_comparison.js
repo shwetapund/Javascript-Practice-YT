ed9d155148b8e563200986cc213880d865544cf3
shwetapund <pundshweta2020@gmail.com>
2024-04-07 01:46:26 -0700

    comparison practice

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>3);
+// console.log(4<5);
+// console.log(4<=5);
+// console.log(4>=5);
+
+// console.log("2" > 3);
+// console.log("03" > 1);
+
+console.log(null > 0); //null ki value empty hoti hein
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ye null & undefined ke comparison ko avoid he krte hein
+
+//comparison & euiality check ye alag alag chije hein
+
+//strict check :- === 
+console.log("2" === 2);
