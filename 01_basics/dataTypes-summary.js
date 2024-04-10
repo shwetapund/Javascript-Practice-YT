935c52a3666ea08f17cc627bd1fc634f4caa8971
shwetapund <pundshweta2020@gmail.com>
2024-04-10 07:33:12 -0700

    write short notes & change in location

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -2,7 +2,8 @@
 
 /** / 7 categories : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-//***null*** mtlb empty ye 0 nahi ye ye empty string bhi nahi hein
+//***null*** mtlb empty ye 0 nahi ye ye empty string bhi nahi hein 
+null ek *object* return karta hein
 
 //***undefined*** mtbl variable declare kiya hein par abhi value assign nahi ki hein
 
@@ -21,9 +22,9 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 //symbol: symbol mein hum value pass krte hein jo b=humein return type milta hein vo bhi alga type ka hi hota heein
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
-const bigNum = 333333333333333333666555n
+// const bigNum = 333333333333333333666555
 
 /**/ // Reference (Non primitive) datatype
     // inka memory mein mein direct reference directly allocate kiya ja sakta hein 
@@ -31,6 +32,23 @@ const bigNum = 333333333333333333666555n
         // Array, Objects, functions 
 
 const heros =[ "sd", "ffg"] //array
+
 let dimObj = {
     CGG: "dc"
 } //objects
+
+const myFunction = function(){
+   // console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
+//function actually function hi return karta hein but isko kehet hein **objectFunction**
+//function ke return specialist ko objectfunction bola jata hein 
+//jitne bhi non primitive type hein inka jo return type ata hein wo obj hi ata hein 
+
+//retunr type
+//**********************************************************
+//null = Object
+//function = function , objectFunction
+//undefined = undefined
