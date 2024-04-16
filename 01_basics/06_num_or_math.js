9898e3d63ee8fd1a2c797c0d87c5d0f05add340e
shwetapund <pundshweta2020@gmail.com>
2024-04-16 01:04:09 -0700

    math and numver practice

---
+++ 01_basics/06_num_or_math.js
@@ -0,0 +1,42 @@
+const score = 500
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 255.896576
+
+// console.log(otherNum.toPrecision(3))
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);                //Math ye ek apne ap mein ek **object** hein aur       ismein bohot sare properties hein
+// console.log(Math.abs(4)); 
+// //abs** mtlb ye ek absolute value hein ye negative(-) value ko positive(+) mein convert kr deta hein **agar hum positive bhi denge tub bhi positive hi value ayengi
+// console.log(Math.round(4.6));
+// //round ** :- ye value ko roundup krdeta hein 
+// console.log(Math.ceil(4.6));      //top mein convert kr deta hein value ko
+// console.log(Math.floor(4.9));     //small value mein convert kr deta hein value ko
+// console.log(Math.min(3,4,5,6,7));
+// console.log(Math.max(3,4,5,6,7));
+
+console.log(Math.random()); 
+console.log((Math.random()*10) + 1); 
+console.log(Math.floor(Math.random()*10) + 1); 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+//math.random ki value humesha ayegi 0 or 1 ke bich mein
+
