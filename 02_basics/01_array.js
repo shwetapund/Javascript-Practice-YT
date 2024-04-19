1c7186f59aded49a9cbabe693edf840d5fcf5709
shwetapund <pundshweta2020@gmail.com>
2024-04-19 10:36:37 -0700

    array basic practice and write notes

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5, true, "shweta"]
+console.log(myArr[0]);
+
+const mygods = ["ganpati", "shiv", "ram"]
+
+const myName = new Array(1,2,2,3)
+
+
+//** the array is a object which is store a collection of multiple items like number, string, bollean, objects, arrays value  in a single variable
+
+//** array elements can not be access using arbitrary strings as indexes
+// ** array index starts with 0
+
+//** js array-copy operation creates **shallow copies** 
+
+//** Shallow copies : shallow copy is an object whose property share the same reference point 
+//** Deep Copy : A deep copy is an  object is a copy whose property do not share the same reference
+
+myArr.push(6) //array method
+console.log(myArr);
