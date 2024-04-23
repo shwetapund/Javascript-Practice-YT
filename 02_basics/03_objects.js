d566f5f42a61d4ef1f65f5ff8abc011528399d20
shwetapund <pundshweta2020@gmail.com>
2024-04-22 22:39:15 -0700

    practice with new object concept

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,11 +2,12 @@
 
 //object literals
 
-const mySymbol = Symbol("key1")
+const mySymbol = Symbol("key1") //symbol declare
 
 const JsUser = {
     name: "shweta",
-    "age smae": "21",
+    age: "21",
+    [mySymbol]:"mykey1", //symbol access with key and value
     location: "Nagar💖",
     email: "shweta@gmail.com",
     isLoggedIn: false,
@@ -15,11 +16,27 @@ const JsUser = {
 
 //Object.create ==> **object aise bhi bna sakte hein isko hi constructor bola jata hein aur iske andar singlton bnt hein 
 
-console.log(JsUser.email) //object access tarika 1
-console.log(JsUser["email"]); //object access tarika 2
-console.log(JsUser["age smae"]);
+// console.log(JsUser.email) //object access tarika 1
+// console.log(JsUser["email"]); //object access tarika 2
 
+// console.log( JsUser[mySymbol]); //symbol print
 
+JsUser.email = "shweta123@g ail.com" //value ko change krna
+// Object.freeze(JsUser) //freeza karna cc=mtlb hum usko fix krte hein uski value ki iske bad ap value kroge tub bhi mein change nahi karne dungi
+JsUser.email = "shweta345@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello js user");
+}
+// console.log(JsUser.greeting); //aisa kiya to function execute nahi hua hein sirf function ka ref ayah hien 
+
+ 
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello js user, ${this.name}`); //humine same obj ko ref karna hein to hum likh sakte hein **this ke bad . lagaynege to jo bhi huamra object hein uske andar jiti properties hein wo humein sub mil jayenge eg this.name
+}
+console.log(JsUser.greetingTwo()); 
 
 
 
