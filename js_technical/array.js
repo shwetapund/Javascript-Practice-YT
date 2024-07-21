71d197d8412124aecd298a37cd1f131c2dd9eaa4
shwetapund <pundshweta2020@gmail.com>
2024-07-21 01:24:25 -0700

    practic with js array technical que

--- js_technical/array.js
+++ js_technical/array.js
@@ -2,10 +2,41 @@
 
 //======>
 
-const array = [2,5,6,3,3,2,5]
-const duplicate = array.filter((ele, index, arr)=>arr.indexOf(ele)!==index)
-console.log(duplicate); //find the duplicated elements
+// const array = [2,5,6,3,3,2,5]
+// const duplicate = array.filter((ele, index, arr)=>arr.indexOf(ele)!==index)
+// console.log(duplicate); //find the duplicated elements
 
-const array1 = [2,5,6,3,3,2,5]
-const orignial = array1.filter((ele, index, arr)=>arr.indexOf(ele)===index)
-console.log(orignial); //find the original elements
+// const array1 = ["shweta", "shital", "shweta", "datta"]
+// const orignial = array1.filter((ele, index, arr)=>arr.indexOf(ele)!==index)
+// console.log(orignial); //find the original elements
+
+//Question 2 How to find the max and min value in a given array in js?
+//========>
+
+// const array = [3,"shweta",4,"shital",4,5,2,6]
+// const duplictes = array.filter((item, index,arr)=>arr.indexOf(item)!==index)
+// console.log(duplictes);
+
+// const orignial = [...new Set(array)]
+// console.log(orignial);
+
+//***************************************Array imp que *********************/
+
+//Q 1: ek array lijjye aur us ke andr kuch additional properties hein apko sirf uski original properties ko hi loop ke nadar display krna hein ?
+
+Array.prototype.extraProperty = "shweta"
+const arrayone = [2,3,5,6,4,1]
+for(let v in arrayone){
+    console.log(v);
+} 
+//Agar aisa kiya to orginal or extra property bhi display hoti hein
+
+//*********************hasOwnProperty************************* 
+
+for(let v in arrayone){
+    if(arrayone.hasOwnProperty){
+        console.log(v);
+    }
+}
+
+
---
+++ js_technical/ternory.js
@@ -0,0 +1,5 @@
+//ternory operator exampls 
+//Question 1 
+const age = 20;
+const check = age >= 18 ? "Yes" : "no";
+console.log(check);
